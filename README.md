f7ea21ab0384272d32a218cb1e5d6d906ecab76d
brayang26 <brayang26_@hotmail.com>
2018-05-18 23:24:53 +0000

    Envío de datos al servidor

--- README.md
+++ README.md
@@ -8,5 +8,6 @@ Visualice aqui el [CRONOGRAMA](https://docs.google.com/spreadsheets/d/1RLBRFkarL
 [Ver aplicación](https://my-events-project-brayang26.c9users.io/index.html)
 
 ![keep working](https://www.ea-coder.com/wp-content/uploads/2015/07/can-any-strategy-be-coded-into-mt4-robot-704x450.jpg)
+![thinking](https://78.media.tumblr.com/76a24d12bf7e57c5048c6838ca6fd79e/tumblr_oglodqrnFy1ve01pfo1_500.gif)
 
 
--- js/index.js
+++ js/index.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
     });
 
     $("#enviarDatos").click(recogerDatos);
-
+    $('#recibirDatos').click(recibirDatos);
 });
 
 function recogerDatos() {
@@ -34,22 +34,42 @@ function recogerDatos() {
 }
 
 function enviarDatos(email, organizador) {
-    alert("Email: "+email+"\n"+"Organizador: "+organizador);
-    
-    var JSONObject= {"email":email, "organizador":organizador };
-    var jsonData = JSON.parse( JSONObject );    
+    var urlAPI = 'https://script.google.com/macros/s/AKfycbzZKsQdwLaPvxD-Vt-HC_4sAnlNAjJQ5p925lVWqAHSCwAENJ4/exec';
+    // alert("Email: " + email + "\n" + "Organizador: " + organizador);
 
+    var JSONObject = { "id":5,
+                        "organizador": organizador,
+                        "email": email
+                    };
+    // var jsonData = JSON.parse(JSONObject);
     var request = $.ajax({
-        url: "rest/orders",
-        type: "POST",
-        data: jsonData,
+        type:'POST',
+        url: urlAPI,
+        data: JSONObject,
+        contentType: "application/json; charset=utf-8",
         dataType: "json",
-        processData:true,
-        success:function(data,status,jqXHR){
-            alert("success..."+data);
+        processData: true,
+        success: function (data, status, jqXHR) {
+            alert("success..." + data);
         }
-    }); 
+    });
 }
+
+function recibirDatos(){
+    var URL = 'https://script.google.com/macros/s/AKfycbzZKsQdwLaPvxD-Vt-HC_4sAnlNAjJQ5p925lVWqAHSCwAENJ4/exec';
+    var action = '?action=get&prodid=1&prodid=2';
+    
+    var request = $.ajax({
+        url: URL+action,
+        data:{format:'json'},
+        error:function(){alert('no se pudo')},
+        dataType:'jsonp',
+        success:function(data){alert(data);},
+        type:'GET',
+        
+    });
+}
+
 
 
 
